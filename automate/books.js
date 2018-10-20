f91f63a8d0596a06e146ce4c1edf41a221475f21
Rob Lindsey <rob@roblindsey.com>
2018-10-20 14:36:33 -0400

    polished books.js. new blog post. css tweaks

--- automate/books.js
+++ automate/books.js
@@ -75,7 +75,7 @@ function getNewToken(oAuth2Client, callback) {
   });
 }
 
-/* Get last date read */
+// Helper function for getting date of last book in books.json
 function getLastDateRead() {
   let booksJSON = require('../src/_data/books.json');
   let dateRead = dayjs(booksJSON.read[0].books[0].dateRead).format(
@@ -86,11 +86,13 @@ function getLastDateRead() {
 
 /* Get GoodReads Data */
 function getGoodReadsData(lastDate) {
+  // Filter function for array filter
   function filterByDate(item) {
     let dateRead = dayjs(item.read_at._text).format('YYYY/MM/DD');
     let dateReadRounded = new Date(dateRead);
     return lastDate < dateReadRounded;
   }
+  // Returns promise
   return rp(
     'https://www.goodreads.com/review/list/1665822.xml?key=AoMooNgSusmmmJbD1az9lg&v=2&shelf=read&sort=date_read&per_page=10'
   )
@@ -101,13 +103,13 @@ function getGoodReadsData(lastDate) {
       var booksRaw = json.filter(filterByDate);
       var books = booksRaw.map(review => {
         var dateRead = new Date(review.read_at._text);
-        return {
-          title: review.book.title._text,
-          author: review.book.authors.author.name._text,
-          isbn: review.book.isbn._text,
-          rating: review.rating._text,
-          dateRead: dayjs(dateRead).format('YYYY/MM/DD'),
-        };
+        return [
+          review.book.title._text,
+          review.book.authors.author.name._text,
+          review.book.isbn._text,
+          review.rating._text,
+          dayjs(dateRead).format('YYYY/MM/DD'),
+        ];
       });
 
       return books;
@@ -115,89 +117,109 @@ function getGoodReadsData(lastDate) {
     .catch(err => console.log(err));
 }
 
-/* Update Google Sheet */
-function updateSheet(auth) {
+function appendToSheet(auth, spreadsheetID, array) {
   const sheets = google.sheets({ version: 'v4', auth });
 
+  // Returns promise
+  return sheets.spreadsheets.values.append({
+    spreadsheetId: spreadsheetID,
+    range: '2018',
+    valueInputOption: 'RAW',
+    resource: {
+      range: '2018',
+      majorDimension: 'ROWS',
+      values: array,
+    },
+  });
+}
+
+function sortSheet(auth, spreadsheetID, sheetID) {
+  const sheets = google.sheets({ version: 'v4', auth });
+
+  // Returns promise
+  return sheets.spreadsheets.batchUpdate({
+    spreadsheetId: spreadsheetID,
+    resource: {
+      requests: [
+        {
+          sortRange: {
+            range: {
+              sheetId: sheetID,
+              startRowIndex: 0,
+              startColumnIndex: 0,
+            },
+            sortSpecs: [
+              {
+                dimensionIndex: 4,
+                sortOrder: 'DESCENDING',
+              },
+            ],
+          },
+        },
+      ],
+    },
+  });
+}
+
+function getSheetValues(auth, spreadsheetID, ranges) {
+  const sheets = google.sheets({ version: 'v4', auth });
+
+  // Returns promise
+  return sheets.spreadsheets.values.batchGet({
+    spreadsheetId: spreadsheetID,
+    ranges: ranges,
+  });
+}
+
+function writeBooksJSON(sheetData) {
+  const rowsArray = sheetData.data.valueRanges;
+  // Map array by years
+  const years = rowsArray.map(year => {
+    let yearObj = {};
+    // Grab year value for keys in new array
+    yearObj.year = year.values[0][4].split('/')[0];
+    // Map books to year arrays
+    yearObj.books = year.values.map(row => {
+      return {
+        title: row[0],
+        author: row[1],
+        isbn: row[2],
+        rating: row[3],
+        dateRead: row[4],
+      };
+    });
+    return yearObj;
+  });
+  // Add years arrays to books object
+  const books = { read: years };
+  const jsonString = JSON.stringify(books);
+
+  // Write JSON file
+  fs.writeFile('../src/_data/books.json', jsonString, err => {
+    if (err) {
+      console.log(err);
+    }
+    console.log('Books datafile updated!');
+  });
+}
+
+/* Update Google Sheet */
+function updateSheet(auth) {
   const lastDate = getLastDateRead();
   getGoodReadsData(lastDate)
     .then(res => {
-      return res.map(book => {
-        return [book.title, book.author, book.isbn, book.rating, book.dateRead];
-      });
+      return appendToSheet(auth, grSheetId, res);
+    })
+    .then(() => {
+      const sheet2018 = 1849336083;
+      return sortSheet(auth, grSheetId, sheet2018);
+    })
+    .then(() => {
+      const ranges = ['2018', '2017', '2016'];
+      return getSheetValues(auth, grSheetId, ranges);
     })
     .then(res => {
-      return sheets.spreadsheets.values.append({
-        spreadsheetId: grSheetId,
-        range: '2018',
-        valueInputOption: 'RAW',
-        resource: {
-          range: '2018',
-          majorDimension: 'ROWS',
-          values: res,
-        },
-      });
-    })
-    .then(() => {
-      return sheets.spreadsheets.batchUpdate({
-        spreadsheetId: grSheetId,
-        resource: {
-          requests: [
-            {
-              sortRange: {
-                range: {
-                  sheetId: 1849336083,
-                  startRowIndex: 0,
-                  startColumnIndex: 0,
-                },
-                sortSpecs: [
-                  {
-                    dimensionIndex: 4,
-                    sortOrder: 'DESCENDING',
-                  },
-                ],
-              },
-            },
-          ],
-        },
-      });
-    })
-    .then(() => {
-      return sheets.spreadsheets.values.batchGet(
-        {
-          spreadsheetId: grSheetId,
-          ranges: ['2018', '2017', '2016'],
-        },
-        (err, res) => {
-          if (err) return console.log('The API returned an error: ' + err);
-
-          const rows = res.data.valueRanges;
-          if (rows.length) {
-            const years = rows.map(year => {
-              let yearObj = {};
-              yearObj.year = year.values[0][4].split('/')[0];
-              yearObj.books = year.values.map(row => {
-                return {
-                  title: row[0],
-                  author: row[1],
-                  isbn: row[2],
-                  rating: row[3],
-                  dateRead: row[4],
-                };
-              });
-              return yearObj;
-            });
-            const books = { read: years };
-            const jsonString = JSON.stringify(books);
-            fs.writeFile('../src/_data/books.json', jsonString, err => {
-              if (err) {
-                console.log(err);
-              }
-              console.log('Books datafile updated!');
-            });
-          }
-        }
-      );
+      return writeBooksJSON(res);
     })
     .catch(err => console.log(err));
 }
