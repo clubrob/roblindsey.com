806c80b6d4ab69f421841c4ceacc9ef2caf97c93
Rob Lindsey <rob@roblindsey.com>
2025-02-13 13:48:31 -0500

    mobile view for cms

--- src/_data/site/info.js
+++ src/_data/site/info.js
@@ -2,7 +2,7 @@ export default function () {
 	return {
 		name: "Rob Lindsey",
 		title: "Rob Lindsey",
-		url: "https://roblindsey.com",
+		url: "https://www.roblindsey.com",
 		description:
 			"Rob Lindsey is a web guy in western NC. Music, books, and other stuff.",
 	};
