2d50261ebeb1d1782dc173b02923d2fa829e3a93
Rob Lindsey <rob@roblindsey.com>
2025-02-13 14:21:49 -0500

    removed www from urls

--- src/_data/site/info.js
+++ src/_data/site/info.js
@@ -2,7 +2,7 @@ export default function () {
 	return {
 		name: "Rob Lindsey",
 		title: "Rob Lindsey",
-		url: "https://www.roblindsey.com",
+		url: "https://roblindsey.com",
 		description:
 			"Rob Lindsey is a web guy in western NC. Music, books, and other stuff.",
 	};
