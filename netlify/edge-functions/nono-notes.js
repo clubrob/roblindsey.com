306f1b8f0e054f90678f8b251fbe1f618dca3774
Rob Lindsey <rob@roblindsey.com>
2025-02-16 15:24:07 -0500

    new note

--- netlify/edge-functions/nono-notes.js
+++ netlify/edge-functions/nono-notes.js
@@ -1,17 +1,27 @@
-export default async function (req) {
-	const ua = req.headers.get("user-agent");
+export default async (request) => {
+	const ua = request.headers.get("user-agent");
 	const uaList = ["Mastodon", "Bridgy", "Friendica", "Pleroma"];
 
+	console.log(`Received request with User-Agent: ${ua}`);
+
 	if (!ua) {
+		console.log("No User-Agent found, continuing request");
 		return;
 	}
 
-	if (uaList.some((u) => u.includes(ua))) {
-		return new Response(null, {
-			status: 204,
-		});
+	const lowerUA = ua.toLowerCase();
+	if (uaList.some((u) => lowerUA.includes(u.toLowerCase()))) {
+		console.log(`Blocked request from ${ua}`);
+		return new Response(null, { status: 204 });
 	}
-}
+
+	// if (uaList.some((u) => u.includes(ua))) {
+	// 	return new Response(null, {
+	// 		status: 204,
+	// 	});
+	// }
+	console.log("Request allowed to continue");
+};
 
 export const config = {
 	onError: "bypass",
